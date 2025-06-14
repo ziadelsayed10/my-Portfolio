0913ba141893eee988b2bb2ff92f8886336e2f99
ziad elsayed10e <ziooziad@gmail.com>
2025-06-14 23:38:58 +0300

    third commit download cv problem

--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -7,7 +7,7 @@ import { fadeIn } from '../../framerMotion/variants';
 import { FaFacebookSquare } from 'react-icons/fa';
 
 function Hero() {
-    const fileUrl = "/public/photo/Ziad Elsayed cv.pdf" 
+    const fileUrl = "/public/photo/ZiadElsayedcv.pdf" 
 
   return (
     <>
